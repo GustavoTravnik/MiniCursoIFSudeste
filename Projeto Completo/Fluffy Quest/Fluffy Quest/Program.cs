57fd35e0033d26d6209fc35c302a18972283e63b
sabidao <x-sabidao@hotmail.com>
2016-01-07 10:26:11 -0200

    criado projetos

---
+++ Projeto Completo/Fluffy Quest/Fluffy Quest/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Fluffy_Quest
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Principal game = new Principal())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
